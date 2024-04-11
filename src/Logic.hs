838c923762bd76950299b07f61934a31ea78adc1
MorveN11 <morales.patty.jose@gmail.com>
2024-04-11 11:09:40 -0400

    feat: add random value after movement

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,18 +1,40 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Logic (updateModel) where
+module Logic (updateModel, getIndexOfEmpties) where
 
+import Constants (size, twoPercentChance)
 import Data.List (partition)
-import Game (Action (..), Model (..), Tile (..))
+import Game (Action (..), Board, Model (..), Tile (..))
 import Miso (Effect, noEff)
 import Miso.Subscription.Keyboard (Arrows (..))
-import Utils (transpose)
+import Utils (chop, getRandomInt, getValueOfVectorIndex, transpose)
+
+updateTile :: Board -> Int -> Int -> Board
+updateTile grid index value =
+  chop size (xs ++ [Tile value] ++ tail ys)
+  where
+    (xs, ys) = splitAt index (concat grid)
+
+getIndexOfEmpties :: Board -> [Int]
+getIndexOfEmpties grid = [i | (i, x) <- zip [0 ..] (concat grid), x == Empty]
+
+addRandomTile :: Model -> Model
+addRandomTile Model {..} = Model {board = updateTile board index value, ..}
+  where
+    empties = getIndexOfEmpties board
+    index = getValueOfVectorIndex (getRandomInt (0, length empties - 1)) empties
+    value = if getRandomInt (1, 10) <= twoPercentChance then 2 else 4
 
 updateModel :: Action -> Model -> Effect Action Model
 updateModel Initialize m = noEff m
-updateModel (ArrowPress Arrows {..}) Model {..} = noEff (Model {board = move (arrowX, arrowY) board, ..})
+updateModel (ArrowPress Arrows {..}) Model {..} =
+  if board /= board'
+    then noEff (addRandomTile (Model {board = board', ..}))
+    else noEff (Model {..})
+  where
+    board' = move (arrowX, arrowY) board
 
-move :: (Int, Int) -> [[Tile]] -> [[Tile]]
+move :: (Int, Int) -> Board -> Board
 move (x, y) boardGame
   | x == 0 && y == 0 = boardGame
   | x == 0 = transpose (map (move' (y * (-1))) (transpose boardGame))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Game (Board, Tile (Empty, Tile))
+import Logic (getIndexOfEmpties)
+import Test.QuickCheck (Property, property, quickCheck, (===))
+
+grid :: Board
+grid =
+  [ [Empty, Tile 4, Tile 3, Empty],
+    [Empty, Empty, Tile 3, Empty],
+    [Tile 10, Empty, Empty, Tile 3],
+    [Empty, Empty, Empty, Tile 40]
+  ]
+
+testGetIndexOfEmpties :: Property
+testGetIndexOfEmpties = property $ getIndexOfEmpties grid === [0, 3, 4, 5, 7, 9, 10, 12, 13, 14]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Running tests..."
+  quickCheck testGetIndexOfEmpties
+  putStrLn "All tests passed!"
